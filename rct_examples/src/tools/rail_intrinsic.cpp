02f0b430c15979e07384114a7735e1bc7337cc12
Jonathan Meyer <Jmeyer1292@gmail.com>
2018-07-27 00:02:01 -0500

    Adds opencv calib

--- rct_examples/src/tools/rail_intrinsic.cpp
+++ rct_examples/src/tools/rail_intrinsic.cpp
@@ -9,6 +9,10 @@
 #include <opencv2/highgui.hpp>
 #include <ros/ros.h>
 
+// debug
+#include <rct_optimizations/experimental/camera_intrinsic.h>
+#include <opencv2/calib3d.hpp>
+
 namespace
 {
 
@@ -115,6 +119,56 @@ void projectPoints2(const T* const camera_intr, const T* const pt_in_camera, T* 
   pt_in_image[1] = intr.fy() * ypp + intr.cy();
 }
 
+}
+void opencvCameraCalibration(const std::vector<rct_optimizations::CorrespondenceSet>& obs,
+                             const cv::Size& image_size,
+                             const rct_optimizations::CameraIntrinsics& intr)
+{
+  std::vector<std::vector<cv::Vec3f>> object_points;
+  std::vector<std::vector<cv::Vec2f>> image_points;
+
+  for (const auto& o : obs)
+  {
+    std::vector<cv::Vec3f> op;
+    std::vector<cv::Vec2f> ip;
+
+    for (const auto& pair : o)
+    {
+      op.push_back(cv::Vec3f(pair.in_target(0), pair.in_target(1), pair.in_target(2)));
+      ip.push_back(cv::Vec2f(pair.in_image(0), pair.in_image(1)));
+    }
+
+    object_points.push_back(op);
+    image_points.push_back(ip);
+  }
+
+  cv::Mat camera_matrix (3, 3, cv::DataType<double>::type);
+  cv::setIdentity(camera_matrix);
+  camera_matrix.at<double>(0, 0) = intr.fx();
+  camera_matrix.at<double>(1, 1) = intr.fy();
+  camera_matrix.at<double>(0, 2) = intr.cx();
+  camera_matrix.at<double>(1, 2) = intr.cy();
+  cv::Mat dist_coeffs;
+  std::vector<cv::Mat> rvecs;
+  std::vector<cv::Mat> tvecs;
+  cv::calibrateCamera(object_points, image_points, image_size, camera_matrix, dist_coeffs, rvecs, tvecs);
+
+  std::array<double, 4> intr_values;
+  intr_values[0] = camera_matrix.at<double>(0, 0);
+  intr_values[1] = camera_matrix.at<double>(1, 1);
+  intr_values[2] = camera_matrix.at<double>(0, 2);
+  intr_values[3] = camera_matrix.at<double>(2, 2);
+  rct_ros_tools::printCameraIntrinsics(intr_values, "OpenCV Intrinsics");
+  rct_ros_tools::printNewLine();
+
+  std::array<double, 5> dist_values;
+  dist_values[0] = dist_coeffs.at<double>(0);
+  dist_values[1] = dist_coeffs.at<double>(1);
+  dist_values[2] = dist_coeffs.at<double>(2);
+  dist_values[3] = dist_coeffs.at<double>(3);
+  dist_values[4] = dist_coeffs.at<double>(4);
+  rct_ros_tools::printCameraDistortion(dist_values, "OpenCV Distortion");
+  rct_ros_tools::printNewLine();
 }
 
 static
@@ -353,6 +407,14 @@ int main(int argc, char** argv)
       cv::waitKey();
     }
   }
+
+  // OpenCV calib
+  std::vector<rct_optimizations::CorrespondenceSet> corr;
+  for (const auto& scene : data.scenes)
+    for (const auto& s : scene.correspondences)
+      corr.push_back(s);
+
+  opencvCameraCalibration(corr, cv::Size(1920, 1080), intr);
 
   return 0;
 }
