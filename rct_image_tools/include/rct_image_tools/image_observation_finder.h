3d380c91077a8460005b6258810f503f58a0db7d
Douglas Smith <douglas.smith@swri.org>
2019-09-13 09:48:36 -0500

    Allow specifying params for circle detection

--- rct_image_tools/include/rct_image_tools/image_observation_finder.h
+++ rct_image_tools/include/rct_image_tools/image_observation_finder.h
@@ -10,7 +10,7 @@
 
 namespace rct_image_tools
 {
-
+using CircleDetectorParams = CircleDetector::Params;
 /**
  * @brief This class finds 2D observations from images of a known ModifiedCircleGridTarget.
  * All points must be seen or it will fail. Observations are returned in the same order
@@ -22,6 +22,8 @@ public:
   ModifiedCircleGridObservationFinder(const ModifiedCircleGridTarget& target);
 
   boost::optional<std::vector<Eigen::Vector2d>> findObservations(const cv::Mat& image) const;
+  boost::optional<std::vector<Eigen::Vector2d>> findObservations(const cv::Mat& image,
+                                                                 const CircleDetectorParams* params) const;
 
   /**
    * @brief A debugging utility that will draw an observation set onto a copy of a cv::Mat for
@@ -29,11 +31,14 @@ public:
    */
   cv::Mat drawObservations(const cv::Mat& image, const std::vector<Eigen::Vector2d>& observations) const;
 
-  const ModifiedCircleGridTarget& target() const { return target_; }
+  const ModifiedCircleGridTarget& target() const
+  {
+    return target_;
+  }
 
 private:
   ModifiedCircleGridTarget target_;
 };
-}
+}  // namespace rct_image_tools
 
 #endif // RCT_IMAGE_OBSERVATION_FINDER_H
