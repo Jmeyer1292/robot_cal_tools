dd2714ce43c8bc8c996e4a75da4e1c391482816e
ctlewis <colin.lewis@swri.org>
2020-05-19 11:45:40 -0500

    removed commented code

--- rct_optimizations/test/src/pose_generator.cpp
+++ rct_optimizations/test/src/pose_generator.cpp
@@ -70,98 +70,10 @@ std::vector<Eigen::Isometry3d> rct_optimizations::test::genConicalPose(Eigen::Is
 
     camera_positions.push_back(camera_oriented);
     //for world frame need target * position
-    // camera_positions.push_back(focus_ * camera_oriented)?
+    // camera_positions.push_back(target_pose * camera_oriented)?
   }
 
   return camera_positions;
 }
 
 
-//rct_optimizations::test::CorrGen::CorrGen(Eigen::Isometry3d focus, rct_optimizations::test::Target target): focus_(focus)
-//{
-//  setTarget(Target);
-//}
-
-//void rct_optimizations::test::CorrGen::setTarget(rct_optimizations::test::Target target)
-//{
-//  target_ = Target;
-//}
-
-//void rct_optimizations::test::CorrGen::setFocus()
-
-////store isometry vector as private variable as it is an intermediate output (no need to return half baked solution)
-//void rct_optimizations::test::CorrGen::genHemispherePose(double r,
-//                                int theta_cnt,
-//                                int phi_cnt
-//                                )
-//{
-
-//  //all in the target coordinate system, with z forward
-
-//  Eigen::VectorXd theta_range = Eigen::VectorXd::LinSpaced(theta_cnt, 0.0, M_PI);
-//  Eigen::VectorXd phi_range = Eigen::VectorXd::LinSpaced(phi_cnt, 0.0, M_PI);
-
-//  for (long theta_it = 0; theta_it < theta_range.size(); ++theta_it)
-//  {
-//    std::vector<Eigen::Isometry3d> local_scan_path;
-//    local_scan_path.reserve(static_cast<std::size_t>(phi_range.size() * theta_range.size()));
-//    for (long phi_it = 0; phi_it < phi_range.size(); ++phi_it)
-//    {
-
-//      double theta_cur = theta_range(theta_it);
-//      double phi_cur = phi_range(phi_it);
-
-//      //position in target coordinate frame
-//      Eigen::Isometry3d camera = Eigen::Isometry3d::Identity();
-//      camera.translation()(0) = r * std::cos(theta_cur) * std::sin(phi_cur);
-//      camera.translation()(1) = r * std::sin(theta_cur) * std::sin(phi_cur);
-//      camera.translation()(2) = r * std::cos(phi_cur);
-
-//      //camera may not need to be isometry since we only use the translation portion
-//      //x is 'up' in target frame (confirm)
-//      Eigen::Isometry3d camera_oriented = rct_optimizations::test::lookat(camera.translation(), focus_.translation(), Eigen::Vector3d(1, 0, 0));
-
-//      //this vector is still in target spatial coordinates
-//      camera_positions_.push_back(camera_oriented);
-//      //for world frame need target * position
-//      // camera_positions.push_back(focus_ * camera_oriented)?
-//    }
-//  }
-
-//  return;
-//}
-
-
-//void rct_optimizations::test::CorrGen::genConicalPose(int observations,
-//                                                      double r,
-//                                                      double h
-//                                                      )
-
-//{
-//  //Generates positions in target frame; need to convert to world frame
-//  double dt = 2.0f * M_PI / (double)(observations); //delta theta about cone base
-
-//  for(int i = 0; i < observations; ++i)
-//  {
-//    Eigen::Isometry3d camera_pose = Eigen::Isometry3d::Identity();
-//    camera_pose.translation() = focus_.translation();
-
-//    //preserving target spatial coordinate frame:
-//    camera_pose.translate(Eigen::Vector3d{r * cos(i*dt), r*sin(i*dt), h});
-
-//    //change orientation to look at target
-//    Eigen::Isometry3d camera_oriented = rct_optimizations::test::lookat( camera_pose.translation(), focus_.translation(), Eigen::Vector3d(1, 0, 0));
-
-//    camera_positions_.push_back(camera_oriented);
-//    //for world frame need target * position
-//    // camera_positions.push_back(focus_ * camera_oriented)?
-//  }
-
-//  return;
-//}
-
-//void rct_optimizations::test::CorrGen::clearPoses()
-//{
-//  std::vector<Eigen::Isometry3d> clean_pose_vector;
-//  camera_positions_ = clean_pose_vector;
-//}
