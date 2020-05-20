aef6d052f8444170f821325fc5588b59f48ae757
ctlewis <colin.lewis@swri.org>
2020-05-19 23:17:54 -0500

    PR requested changes

--- rct_optimizations/test/include/rct_optimizations_tests/pose_generator.h
+++ rct_optimizations/test/include/rct_optimizations_tests/pose_generator.h
@@ -1,5 +1,4 @@
-#ifndef POSE_GEN_H
-#define POSE_GEN_H
+#pragma once
 
 #include "rct_optimizations_tests/observation_creator.h"
 #include <rct_optimizations_tests/utilities.h>
@@ -12,32 +11,31 @@ namespace test
 {
 /**
  * @brief genHemispherePose Generates camera poses in a hemisphere pattern
- * @param target_pose The position/orientation of the target
+ * @param target_pose The position of the target
  * @param r Radius of the hemisphere
  * @param theta_cnt The number of points in the theta-wise direction
  * @param phi_cnt The number of points in the phi-wise direction
  * @return A vector of camera positions & orientations
  */
-std::vector<Eigen::Isometry3d> genHemispherePose(Eigen::Isometry3d target_pose, double r, int theta_cnt, int phi_cnt);
+std::vector<Eigen::Isometry3d> genHemispherePose(const Eigen::Vector3d& target_pose, const double r, const unsigned int theta_cnt, const unsigned int phi_cnt);
 /**
  * @brief genConicalPose Generates camera positions using a conical template, with the target at the 'point'
- * @param target_pose Position and orientation of the target
+ * @param target_pose The position of the target
  * @param r Radius of the cone
  * @param h Height of the cone (distance to target)
  * @param theta_cnt Number of observations in a theta direction
  * @return A vector of camera positions & orientations
  */
-std::vector<Eigen::Isometry3d> genConicalPose(Eigen::Isometry3d target_pose, int observations, double r, double h);
+std::vector<Eigen::Isometry3d> genConicalPose(const Eigen::Vector3d& target_pose, const unsigned int observations, const double r, const double h);
 
 /**
  * @brief genGridPose Generates camera positions in a grid pattern
- * @param target_pose Position & orientation of the target
+ * @param target_pose The position of the target
  * @param grid_side number of columns & rows to go into grid
  * @param spacing Distance betweeon points
  * @param h Grid distance to target
  * @return A vector of camera positions & orientations
  */
-std::vector<Eigen::Isometry3d> genGridPose(Eigen::Isometry3d target_pose, int grid_side, double spacing, double h);
+std::vector<Eigen::Isometry3d> genGridPose(const Eigen::Vector3d& target_pose, const unsigned int grid_side, const double spacing, const double h);
 }  // namespace test
 }  // namespace rct_optimizations
-#endif  // POSE_GEN
--- rct_optimizations/test/src/pose_generator.cpp
+++ rct_optimizations/test/src/pose_generator.cpp
@@ -5,7 +5,7 @@
 // hemisphere
 
 std::vector<Eigen::Isometry3d>
-rct_optimizations::test::genHemispherePose(Eigen::Isometry3d target_pose, double r, int theta_cnt, int phi_cnt)
+rct_optimizations::test::genHemispherePose(const Eigen::Vector3d& target_pose, const double r, const unsigned int theta_cnt, const unsigned int phi_cnt)
 {
   // all in the target coordinate system, with z forward
   std::vector<Eigen::Isometry3d> camera_positions;
@@ -13,11 +13,11 @@ rct_optimizations::test::genHemispherePose(Eigen::Isometry3d target_pose, double
   Eigen::VectorXd theta_range = Eigen::VectorXd::LinSpaced(theta_cnt, 0.0, M_PI);
   Eigen::VectorXd phi_range = Eigen::VectorXd::LinSpaced(phi_cnt, 0.0, M_PI);
 
-  for (long theta_it = 0; theta_it < theta_range.size(); ++theta_it)
+  for (std::size_t theta_it = 0; theta_it < theta_range.size(); ++theta_it)
   {
     std::vector<Eigen::Isometry3d> local_scan_path;
-    local_scan_path.reserve(static_cast<std::size_t>(phi_range.size() * theta_range.size()));
-    for (long phi_it = 0; phi_it < phi_range.size(); ++phi_it)
+    local_scan_path.reserve(phi_range.size() * theta_range.size());
+    for (std::size_t phi_it = 0; phi_it < phi_range.size(); ++phi_it)
     {
       double theta_cur = theta_range(theta_it);
       double phi_cur = phi_range(phi_it);
@@ -28,15 +28,12 @@ rct_optimizations::test::genHemispherePose(Eigen::Isometry3d target_pose, double
       camera.translation()(1) = r * std::sin(theta_cur) * std::sin(phi_cur);
       camera.translation()(2) = r * std::cos(phi_cur);
 
-      // camera may not need to be isometry since we only use the translation portion
       // x is 'up' in target frame (confirm)
       Eigen::Isometry3d camera_oriented =
-          rct_optimizations::test::lookAt(camera.translation(), target_pose.translation(), Eigen::Vector3d(1, 0, 0));
+          rct_optimizations::test::lookAt(camera.translation(), target_pose, Eigen::Vector3d(1, 0, 0));
 
       // this vector is still in target spatial coordinates
       camera_positions.push_back(camera_oriented);
-      // for world frame need target * position
-      // camera_positions.push_back(focus_ * camera_oriented)?
     }
   }
 
@@ -44,34 +41,32 @@ rct_optimizations::test::genHemispherePose(Eigen::Isometry3d target_pose, double
 }
 
 std::vector<Eigen::Isometry3d>
-rct_optimizations::test::genConicalPose(Eigen::Isometry3d target_pose, int observations, double r, double h)
+rct_optimizations::test::genConicalPose(const Eigen::Vector3d& target_pose, const unsigned int observations, const double r, const double h)
 {
   std::vector<Eigen::Isometry3d> camera_positions;
   // Generates positions in target frame; need to convert to world frame
-  double dt = 2.0f * M_PI / (double)(observations);  // delta theta about cone base
+  double dt = 2.0f * M_PI / double(observations);  // delta theta about cone base
 
   for (int i = 0; i < observations; ++i)
   {
     Eigen::Isometry3d camera_pose = Eigen::Isometry3d::Identity();
-    camera_pose.translation() = target_pose.translation();
+    camera_pose.translation() = target_pose;
 
     // preserving target spatial coordinate frame:
     camera_pose.translate(Eigen::Vector3d{ r * cos(i * dt), r * sin(i * dt), h });
 
     // change orientation to look at target
     Eigen::Isometry3d camera_oriented =
-        rct_optimizations::test::lookAt(camera_pose.translation(), target_pose.translation(), Eigen::Vector3d(1, 0, 0));
+        rct_optimizations::test::lookAt(camera_pose.translation(), target_pose, Eigen::Vector3d(1, 0, 0));
 
     camera_positions.push_back(camera_oriented);
-    // for world frame need target * position
-    // camera_positions.push_back(target_pose * camera_oriented)?
   }
 
   return camera_positions;
 }
 
 std::vector<Eigen::Isometry3d>
-rct_optimizations::test::genGridPose(Eigen::Isometry3d target_pose, int grid_side, double spacing, double h)
+rct_optimizations::test::genGridPose(const Eigen::Vector3d& target_pose, const unsigned int grid_side, const double spacing, const double h)
 {
   // Generates positions in target frame; need to convert to world frame
   std::vector<Eigen::Isometry3d> camera_positions;
@@ -81,19 +76,19 @@ rct_optimizations::test::genGridPose(Eigen::Isometry3d target_pose, int grid_sid
     for (double j = -1 * (grid_side - 1) / 2; j <= (grid_side - 1) / 2; ++j)
     {
       Eigen::Isometry3d camera_pose = Eigen::Isometry3d::Identity();
-      camera_pose.translation() = target_pose.translation();
+      camera_pose.translation() = Eigen::Vector3d{ i * spacing, j * spacing, h };
 
       // preserving target spatial coordinate frame:
       camera_pose.translate(Eigen::Vector3d{ i * spacing, j * spacing, h });
 
       // change orientation to look at target
       Eigen::Isometry3d camera_oriented = rct_optimizations::test::lookAt(
-          camera_pose.translation(), target_pose.translation(), Eigen::Vector3d(1, 0, 0));
+          camera_pose.translation(), target_pose, Eigen::Vector3d(1, 0, 0));
 
       camera_positions.push_back(camera_oriented);
-      // for world frame need target * position
-      // camera_positions.push_back(target_pose * camera_oriented)?
     }
   }
+
+
   return camera_positions;
 }
