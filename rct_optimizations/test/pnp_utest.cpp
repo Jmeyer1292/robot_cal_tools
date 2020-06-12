48a21bf295a45856d214102483fd840bfaa5ebf4
michael.ripperger@swri.org <michael.ripperger@swri.org>
2020-06-11 19:14:43 -0500

    Added print of covariance to unit test

--- rct_optimizations/test/pnp_utest.cpp
+++ rct_optimizations/test/pnp_utest.cpp
@@ -5,18 +5,18 @@
 
 using namespace rct_optimizations;
 
+static const unsigned TARGET_ROWS = 10;
+static const unsigned TARGET_COLS = 14;
+static const double SPACING = 0.025;
+
 TEST(PNP_2D, PerfectInitialConditions)
 {
   test::Camera camera = test::makeKinectCamera();
-
-  unsigned target_rows = 5;
-  unsigned target_cols = 7;
-  double spacing = 0.025;
-  test::Target target(target_rows, target_cols, spacing);
+  test::Target target(TARGET_ROWS, TARGET_COLS, SPACING);
 
   Eigen::Isometry3d target_to_camera(Eigen::Isometry3d::Identity());
-  double x = static_cast<double>(target_rows - 1) * spacing / 2.0;
-  double y = static_cast<double>(target_cols - 1) * spacing / 2.0;
+  double x = static_cast<double>(TARGET_ROWS - 1) * SPACING / 2.0;
+  double y = static_cast<double>(TARGET_COLS - 1) * SPACING / 2.0;
   target_to_camera.translate(Eigen::Vector3d(x, y, 1.0));
   target_to_camera.rotate(Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitX()));
 
@@ -31,20 +31,19 @@ TEST(PNP_2D, PerfectInitialConditions)
   EXPECT_TRUE(result.camera_to_target.isApprox(target_to_camera.inverse()));
   EXPECT_LT(result.initial_cost_per_obs, 1.0e-15);
   EXPECT_LT(result.final_cost_per_obs, 1.0e-15);
+
+  Eigen::IOFormat fmt(4, 0, ", ", "\n", "[", "]");
+  std::cout << "Covariance:\n" << result.camera_to_target_covariance.format(fmt) << std::endl;
 }
 
 TEST(PNP_2D, PerturbedInitialCondition)
 {
   test::Camera camera = test::makeKinectCamera();
-
-  unsigned target_rows = 5;
-  unsigned target_cols = 7;
-  double spacing = 0.025;
-  test::Target target(target_rows, target_cols, spacing);
+  test::Target target(TARGET_ROWS, TARGET_COLS, SPACING);
 
   Eigen::Isometry3d target_to_camera(Eigen::Isometry3d::Identity());
-  double x = static_cast<double>(target_rows) * spacing / 2.0;
-  double y = static_cast<double>(target_cols) * spacing / 2.0;
+  double x = static_cast<double>(TARGET_ROWS) * SPACING / 2.0;
+  double y = static_cast<double>(TARGET_COLS) * SPACING / 2.0;
   target_to_camera.translate(Eigen::Vector3d(x, y, 1.0));
   target_to_camera.rotate(Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitX()));
 
@@ -61,20 +60,19 @@ TEST(PNP_2D, PerturbedInitialCondition)
   EXPECT_TRUE(result.converged);
   EXPECT_TRUE(result.camera_to_target.isApprox(target_to_camera.inverse(), 1.0e-8));
   EXPECT_LT(result.final_cost_per_obs, 1.0e-14);
+
+  Eigen::IOFormat fmt(4, 0, ", ", "\n", "[", "]");
+  std::cout << "Covariance:\n" << result.camera_to_target_covariance.format(fmt) << std::endl;
 }
 
 TEST(PNP_2D, BadIntrinsicParameters)
 {
   test::Camera camera = test::makeKinectCamera();
-
-  unsigned target_rows = 5;
-  unsigned target_cols = 7;
-  double spacing = 0.025;
-  test::Target target(target_rows, target_cols, spacing);
+  test::Target target(TARGET_ROWS, TARGET_COLS, SPACING);
 
   Eigen::Isometry3d target_to_camera(Eigen::Isometry3d::Identity());
-  double x = static_cast<double>(target_rows) * spacing / 2.0;
-  double y = static_cast<double>(target_cols) * spacing / 2.0;
+  double x = static_cast<double>(TARGET_ROWS) * SPACING / 2.0;
+  double y = static_cast<double>(TARGET_COLS) * SPACING / 2.0;
   target_to_camera.translate(Eigen::Vector3d(x, y, 1.0));
   target_to_camera.rotate(Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitX()));
 
@@ -101,6 +99,9 @@ TEST(PNP_2D, BadIntrinsicParameters)
   EXPECT_TRUE(result.converged);
   EXPECT_FALSE(result.camera_to_target.isApprox(target_to_camera.inverse(), 1.0e-3));
   EXPECT_GT(result.final_cost_per_obs, 1.0e-3);
+
+  Eigen::IOFormat fmt(4, 0, ", ", "\n", "[", "]");
+  std::cout << "Covariance:\n" << result.camera_to_target_covariance.format(fmt) << std::endl;
 }
 
 TEST(PNP_3D, PerfectInitialConditions)
