d5a1fa78b1982967fae3851bbe464d6ad61ed098
mripperger <michael.ripperger@swri.org>
2020-06-17 15:03:57 -0500

    Moved chain creation utility to source file

--- rct_optimizations/test/include/rct_optimizations_tests/utilities.h
+++ rct_optimizations/test/include/rct_optimizations_tests/utilities.h
@@ -63,10 +63,10 @@ struct Target
 };
 
 /**
- * @brief perturbPose
- * @param pose
- * @param spatial_noise
- * @param angle_noise
+ * @brief Creates a pose that is perturbed with the input noise levels relative to the input pose
+ * @param pose - reference pose
+ * @param spatial_noise - translational noise standard deviation (m)
+ * @param angle_noise - angular noise standard deviation (rad)
  * @return
  */
 Eigen::Isometry3d perturbPose(const Eigen::Isometry3d &pose,
@@ -77,50 +77,15 @@ Eigen::Isometry3d perturbPose(const Eigen::Isometry3d &pose,
  * @brief Creates a DH parameter-based robot representation of an ABB IRB2400
  * @return
  */
-inline DHChain createABBIRB2400()
-{
-  std::vector<DHTransform::Ptr> joints;
-  joints.reserve(6);
+DHChain createABBIRB2400();
 
-  Eigen::Vector4d t1, t2, t3, t4, t5, t6;
-
-  t1 << 0.615, 0.0, 0.100, -M_PI / 2.0;
-  t2 << 0.0, -M_PI / 2.0, 0.705, 0.0;
-  t3 << 0.0, 0.0, 0.135, -M_PI / 2.0;
-  t4 << 0.755, 0.0, 0.0, M_PI / 2.0;
-  t5 << 0.0, 0.0, 0.0, -M_PI / 2.0;
-  t6 << 0.085, M_PI, 0.0, 0.0;
-
-  joints.push_back(std::make_unique<DHTransform>(t1, DHJointType::REVOLUTE));
-  joints.push_back(std::make_unique<DHTransform>(t2, DHJointType::REVOLUTE));
-  joints.push_back(std::make_unique<DHTransform>(t3, DHJointType::REVOLUTE));
-  joints.push_back(std::make_unique<DHTransform>(t4, DHJointType::REVOLUTE));
-  joints.push_back(std::make_unique<DHTransform>(t5, DHJointType::REVOLUTE));
-  joints.push_back(std::make_unique<DHTransform>(t6, DHJointType::REVOLUTE));
-
-  return DHChain(std::move(joints));
-}
-
-inline DHChain perturbDHCHain(const DHChain &in, const double stddev)
-{
-  std::mt19937 mt_rand(std::random_device{}());
-  std::normal_distribution<double> norm(0.0, stddev);
-
-  // Get the joint types and nominal DH table
-  std::vector<DHJointType> joint_types = in.getJointTypes();
-  Eigen::MatrixX4d dh = in.getDHTable();
-
-  // Perturb each value in the DH table randomly by the input standard deviation
-  dh = dh.unaryExpr([&norm, &mt_rand](const double val) { return val + norm(mt_rand); });
-
-  std::vector<DHTransform::Ptr> transforms;
-  transforms.reserve(joint_types.size());
-  for (std::size_t i = 0; i < joint_types.size(); ++i)
-  {
-    transforms.push_back(std::make_unique<DHTransform>(dh.row(i), joint_types[i]));
-  }
-  return DHChain(std::move(transforms));
-}
+/**
+ * @brief Creates a kinematic chain whose DH parameters are pertubed with Gaussian noise relative to the reference kinematic chain
+ * @param in - reference kinematic chain
+ * @param stddev - standard deviation to apply to all DH parameters individually
+ * @return
+ */
+DHChain perturbDHCHain(const DHChain &in, const double stddev);
 
 } // namespace test
 } // namespace rct_optimizations
--- rct_optimizations/test/src/utilities.cpp
+++ rct_optimizations/test/src/utilities.cpp
@@ -40,6 +40,51 @@ Eigen::Isometry3d perturbPose(const Eigen::Isometry3d& pose, double spatial_nois
   return new_pose;
 }
 
+DHChain createABBIRB2400()
+{
+  std::vector<DHTransform::Ptr> joints;
+  joints.reserve(6);
+
+  Eigen::Vector4d t1, t2, t3, t4, t5, t6;
+
+  t1 << 0.615, 0.0, 0.100, -M_PI / 2.0;
+  t2 << 0.0, -M_PI / 2.0, 0.705, 0.0;
+  t3 << 0.0, 0.0, 0.135, -M_PI / 2.0;
+  t4 << 0.755, 0.0, 0.0, M_PI / 2.0;
+  t5 << 0.0, 0.0, 0.0, -M_PI / 2.0;
+  t6 << 0.085, M_PI, 0.0, 0.0;
+
+  joints.push_back(std::make_unique<DHTransform>(t1, DHJointType::REVOLUTE));
+  joints.push_back(std::make_unique<DHTransform>(t2, DHJointType::REVOLUTE));
+  joints.push_back(std::make_unique<DHTransform>(t3, DHJointType::REVOLUTE));
+  joints.push_back(std::make_unique<DHTransform>(t4, DHJointType::REVOLUTE));
+  joints.push_back(std::make_unique<DHTransform>(t5, DHJointType::REVOLUTE));
+  joints.push_back(std::make_unique<DHTransform>(t6, DHJointType::REVOLUTE));
+
+  return DHChain(std::move(joints));
+}
+
+DHChain perturbDHCHain(const DHChain &in, const double stddev)
+{
+  std::mt19937 mt_rand(std::random_device{}());
+  std::normal_distribution<double> norm(0.0, stddev);
+
+  // Get the joint types and nominal DH table
+  std::vector<DHJointType> joint_types = in.getJointTypes();
+  Eigen::MatrixX4d dh = in.getDHTable();
+
+  // Perturb each value in the DH table randomly by the input standard deviation
+  dh = dh.unaryExpr([&norm, &mt_rand](const double val) { return val + norm(mt_rand); });
+
+  std::vector<DHTransform::Ptr> transforms;
+  transforms.reserve(joint_types.size());
+  for (std::size_t i = 0; i < joint_types.size(); ++i)
+  {
+    transforms.push_back(std::make_unique<DHTransform>(dh.row(i), joint_types[i]));
+  }
+  return DHChain(std::move(transforms));
+}
+
 } // namespace test
 } // namespace rct_optimizations
 
