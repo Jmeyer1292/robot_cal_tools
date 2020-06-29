6aad6e31788e5829199a681244777cea0139c591
ctlewis <colin.lewis@swri.org>
2020-06-28 22:38:10 -0500

    added sampling assertion, moved to optimizations

---
+++ rct_optimizations/include/rct_optimizations/validation/homography_check.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <rct_optimizations/types.h>
+
+namespace rct_optimizations
+{
+/**
+ * @brief checkObservationSequence verifies that the circles on a calibration
+ * target are sequenced correctly using Homography, the mapping of image
+ * points to a plane in the image. This method assumes that the target is
+ * planar and that all target points are visible in the observation. It is the
+ *  user's responsibility to provide a reasonable error threshold.
+ * @param intr Camera intrinsics
+ * @param target Target desciption; rows, cols, and point spacing
+ * @param ob Observation set
+ * @param max_residual Maximium expected error
+ * @return A boolian indicating wheather the target points are sequenced
+ * corectly in the observation
+ */
+bool checkObservationSequence(const rct_optimizations::CameraIntrinsics& intr,
+                      const int& rows,
+                      const int& cols,
+                      const double& spacing,
+                      const rct_optimizations::Observation2D3D& ob,
+                      const double& max_residual);
+
+}//rct_optimizations
