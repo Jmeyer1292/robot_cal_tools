37d3d7f21ffb1f5cee0779ed162294682b4c9050
mripperger <michael.ripperger@swri.org>
2020-06-29 12:40:49 -0500

    Revised noise qualification example

--- rct_examples/src/validation/noise_qualify_2d.cpp
+++ rct_examples/src/validation/noise_qualify_2d.cpp
@@ -1,96 +1,109 @@
-#include <Eigen/Dense>
-#include <rct_optimizations/validation/noise_qualifier.h>
-#include <rct_optimizations/types.h>
-#include <rct_image_tools/image_observation_finder.h>
-#include <stdexcept>
-#include <yaml-cpp/yaml.h>
 #include <opencv/cv.hpp>
-#include <ros/ros.h>
-
-#include <rct_optimizations/pnp.h>
-#include "rct_ros_tools/parameter_loaders.h"
+#include <rct_image_tools/image_observation_finder.h>
+#include <rct_optimizations/validation/noise_qualification.h>
 #include "rct_ros_tools/data_set.h"
+#include "rct_ros_tools/parameter_loaders.h"
+#include <ros/ros.h>
+#include <yaml-cpp/yaml.h>
+
+template<typename T>
+bool get(const ros::NodeHandle &nh, const std::string &key, T &val)
+{
+  if (!nh.getParam(key, val))
+  {
+    ROS_ERROR_STREAM("Failed to get '" << key << "' parameter");
+    return false;
+  }
+  return true;
+}
 
 int main(int argc, char** argv)
 {
+  ros::init(argc, argv, "noise_qualification_2d");
   ros::NodeHandle pnh("~");
 
   // Parse parameters
-  std::string path;
-  if (!pnh.getParam("data_path", path))
+  std::string data_path;
+  if (!get(pnh, "data_path", data_path))
+    return -1;
+
+  std::string data_file;
+  if (!get(pnh, "data_file", data_file))
+    return -1;
+
+  try
   {
-    ROS_ERROR("Must set 'data_path' parameter");
-    return 1;
-  }
+    // Load the target definition and observation finder
+    rct_image_tools::ModifiedCircleGridTarget target = rct_ros_tools::loadTarget(pnh, "target_definition");
+    rct_image_tools::ModifiedCircleGridObservationFinder obs_finder(target);
 
-  int rows;
-  if (!pnh.getParam("rows", rows))
-  {
-    ROS_ERROR("Must set 'rows' parameter");
-    return 1;
-  }
+    // Load camera intrinsics
+    rct_optimizations::CameraIntrinsics camera = rct_ros_tools::loadIntrinsics(pnh, "intrinsics");
 
-  int cols;
-  if (!pnh.getParam("columns", cols))
-  {
-    ROS_ERROR("Must set 'columns' parameter");
-    return 1;
-  }
+    // Load an initial guess for the camera to target transformation
+    Eigen::Isometry3d camera_to_target_guess = rct_ros_tools::loadPose(pnh, "camera_to_target_guess");
 
-  //make target
-  rct_image_tools::ModifiedCircleGridTarget target (rows, cols, 0.01);
-  rct_image_tools::ModifiedCircleGridObservationFinder obs_finder(target);
+    // Load the data file which specifies the location of the images on which to perform the noise qualification
+    YAML::Node root = YAML::LoadFile(data_file);
 
-  YAML::Node root = YAML::LoadFile(path + std::string("/data.yaml"));
-
-  //Load camera intrinsics
-  rct_optimizations::CameraIntrinsics camera = rct_ros_tools::loadIntrinsics(path +  std::string("/camera_data.yaml"));
-
-  //reserve observations
-  std::vector<rct_optimizations::PnPProblem> problem_set;
-  problem_set.reserve(root.size());
-
-  //create observations
-  for (std::size_t i = 0; i < root.size(); ++i)
-  {
-    rct_optimizations::PnPProblem problem;
-    problem.intr = camera;
-
-    const auto pose_path = root[i]["pose"].as<std::string>();
-    problem.camera_to_target_guess = rct_ros_tools::loadPose(path + pose_path);
-
-    // Each entry should have a pose and image path. This path is relative to the root_path directory!
-    const auto img_path = root[i]["image"].as<std::string>();
-    const std::string image_name = path + img_path;
-    static cv::Mat image = rct_ros_tools::readImageOpenCV(image_name);
-
-    auto maybe_obs = obs_finder.findObservations(image);
-    if (!maybe_obs)
+    // Set up the noise qualification inputs
+    std::vector<rct_optimizations::PnPProblem> problem_set;
+    problem_set.reserve(root.size());
+    for (std::size_t i = 0; i < root.size(); ++i)
     {
-      cv::imshow("points", image);
-      cv::waitKey();
-      continue;
+      // Each entry should have an image path. This path is relative to the root_path directory!
+      const auto img_path = root[i]["image"].as<std::string>();
+      const std::string image_name = data_path + "/" + img_path;
+      static cv::Mat image = rct_ros_tools::readImageOpenCV(image_name);
+
+      // Find the observations in the image
+      auto maybe_obs = obs_finder.findObservations(image);
+      if (!maybe_obs)
+      {
+        cv::imshow("points", image);
+        ROS_INFO_STREAM("Hit enter in the OpenCV window to continue");
+        cv::waitKey();
+        continue;
+      }
+      else
+      {
+        // Show the points we detected
+        cv::imshow("points", obs_finder.drawObservations(image, *maybe_obs));
+        ROS_INFO_STREAM("Hit enter in the OpenCV window to continue");
+        cv::waitKey();
+      }
+
+      // Set up the PnP problem for this image
+      rct_optimizations::PnPProblem problem;
+      problem.intr = camera;
+      problem.camera_to_target_guess = camera_to_target_guess;
+
+      // Add the detected correspondences
+      problem.correspondences.reserve(maybe_obs->size());
+      for (std::size_t j = 0; j < maybe_obs->size(); ++j)
+      {
+        problem.correspondences.emplace_back(maybe_obs->at(j), target.points.at(j));
+      };
+
+      problem_set.push_back(problem);
     }
 
-    // And loop over each detected dot:
-    for (std::size_t j = 0; j < maybe_obs->size(); ++j)
-    {
-      problem.correspondences[j].in_image = maybe_obs->at(j); // The obs finder and target define their points in the same order!
-      problem.correspondences[j].in_target = target.points[j];
-    };
+    // Perform the noise qualification
+    rct_optimizations::PnPNoiseStat result = rct_optimizations::qualifyNoise2D(problem_set);
+
+    // Print the results
+    Eigen::IOFormat fmt(4, 0, ",", "\n", "[", "]");
+    ROS_INFO_STREAM("Camera to Target Noise Results");
+    ROS_INFO_STREAM("Position mean (m)\n" << result.p_stat.mean.transpose().format(fmt));
+    ROS_INFO_STREAM("Position standard deviation (m)\n" << result.p_stat.stdev.transpose().format(fmt));
+    ROS_INFO_STREAM("Quaternion mean (qx, qy, qz, qw)\n" << result.q_stat.mean.coeffs().transpose().format(fmt));
+    ROS_INFO_STREAM("Quaternion standard deviation\n" << result.q_stat.stdev);
+  }
+  catch (const std::exception &ex)
+  {
+    ROS_ERROR_STREAM(ex.what());
+    return -1;
   }
 
-  //start with a good guess
-
-  rct_optimizations::PnPNoiseStat output = rct_optimizations::qualifyNoise2D(problem_set);
-
-
-  ROS_INFO_STREAM("The standard deviation is : x: " << output.x.std_dev <<
-                                           "\n y: " << output.y.std_dev <<
-                                           "\n z: " << output.z.std_dev <<
-                                           "\n i: " << output.q.qx.std_dev <<
-                                           "\n j: " << output.q.qy.std_dev <<
-                                           "\n k: " << output.q.qz.std_dev <<
-                                           "\n w: " << output.q.qw.std_dev <<
-                                                                      "\n");
+  return 0;
 }
