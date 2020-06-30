25fb6c5a8c2945ba5d71844f74c666f24038954f
mripperger <michael.ripperger@swri.org>
2020-06-30 18:01:58 -0500

    Updates to homography error calculation

--- rct_optimizations/include/rct_optimizations/validation/homography_check.h
+++ rct_optimizations/include/rct_optimizations/validation/homography_check.h
@@ -3,22 +3,74 @@
 
 namespace rct_optimizations
 {
-/**
- * @brief checkObservationSequence verifies that the circles on a calibration
- * target are sequenced correctly using Homography, the mapping of image
- * points to a plane in the image. This method assumes that the target is
- * planar and that all target points are visible in the observation. It is the
- *  user's responsibility to provide a reasonable error threshold.
- * @param intr Camera intrinsics
- * @param target Target desciption; rows, cols, and point spacing
- * @param ob Observation set
- * @param max_residual Maximium expected error
- * @return A boolian indicating wheather the target points are sequenced
- * corectly in the observation
- */
-bool checkObservationSequence(const int& rows,
-                              const int& cols,
-                              const rct_optimizations::Observation2D3D& ob,
-                              const double& max_residual);
 
-}//rct_optimizations
+/**
+ * @brief A simple structure for choosing which correspondence indices to use for generating the homography matrix
+ */
+struct CorrespondenceSampler
+{
+  virtual std::vector<std::size_t> getSampleCorrespondenceIndices() const = 0;
+};
+
+/**
+ * @brief A correspondence sampler specifically for modified circle grid targets
+ */
+struct ModifiedCircleGridCorrespondenceSampler : CorrespondenceSampler
+{
+  ModifiedCircleGridCorrespondenceSampler(const std::size_t rows_, const std::size_t cols_)
+    : rows(rows_)
+    , cols(cols_)
+  {
+  }
+
+  virtual std::vector<std::size_t> getSampleCorrespondenceIndices() const final override
+  {
+    // Make sure there are at least two times as many points as the number of sample points
+    if ((rows * cols / 2) < 8)
+    {
+      std::stringstream ss;
+      ss << "Number of correspondences does not exceed minimum of 16 (" << rows * cols << " provided)";
+      throw std::runtime_error(ss.str());
+    }
+
+    // For a modified circle target grid, these points should be the corners of the grid
+    std::vector<std::size_t> correspondence_indices;
+    correspondence_indices.reserve(8);
+
+    // Corner Points
+    correspondence_indices.push_back(0); // upper left point
+    correspondence_indices.push_back(rows - 1); // upper right point
+    correspondence_indices.push_back(rows * cols - cols - 1); // lower left point
+    correspondence_indices.push_back(rows * cols - 1); // lower right point
+
+    // Points near the corners of the modified circle grid (increases the accuracy of the homography matrix calculation when there is noise)
+    correspondence_indices.push_back(1); // nearby to point1
+    correspondence_indices.push_back(rows - 2); // nearby to point2
+    correspondence_indices.push_back(rows * cols - cols - 2); // nearby to point3
+    correspondence_indices.push_back(rows * cols - 2); // nearby to point4
+
+    return correspondence_indices;
+  }
+
+  std::size_t rows;
+  std::size_t cols;
+};
+
+/**
+ * @brief Computes the error between a set of corresponding planar data points (points on a planar target and their corresponding locations in the image plane)
+ * This function creates a homography matrix which transforms data from one plane onto another plane (i.e. from the target plane to the image plane).
+ * This matrix is created using only a subset of the correspondences. The matrix is then used to estimate the location of all target points in the image plane.
+ * The error between these estimates and the actual values is a good measurement of how accurately a set of 2D camera measurements represent a calbration target with known geometry
+ *
+ * Assumptions:
+ *  - Both sets of points lie on a plane (i.e. points on a planar calibration target and points on the image plane)
+ *
+ * @param correspondences - A set of corresponding points
+ * @param correspondence_sampler - a struct for choosing correspondence indices to generate the homography matrix
+ * @return A vector of homography errors for each correspondence
+ * @throws Exception when number of correspondences does not exceed 2x the number of samples
+ */
+Eigen::VectorXd calculateHomographyError(const Correspondence2D3D::Set &correspondences,
+                                         const CorrespondenceSampler &correspondence_sampler);
+
+} //rct_optimizations
--- rct_optimizations/src/rct_optimizations/validation/homography_check.cpp
+++ rct_optimizations/src/rct_optimizations/validation/homography_check.cpp
@@ -1,119 +1,107 @@
 #include <rct_optimizations/validation/homography_check.h>
 #include <Eigen/Dense>
+#include <rct_optimizations/types.h>
 
-bool checkObservationSequence(const int& rows,
-                              const int& cols,
-                              rct_optimizations::Observation2D3D& ob,
-                              const double& max_residual)
+namespace rct_optimizations
 {
-
-  /*points used for homography; cannot represent more than 1/2
-  * target points (to keep an adequate validaiton set) and cannot
-  * be less than 4
-  */
-  int sampled_points = 4;
-
-  //Ensure that there are enough points for testing outside of the sampled set
-  assert(rows * cols > 2*sampled_points);
-
-  //Alpha is used to find the unique elements of the homography matrix
-  Eigen::MatrixXd Alpha(8,1);
-
-  /*The reduced form of the homography relations of a point x,y and its image
-   * representation U,V. Used to find the Homography matrix components.
+Eigen::VectorXd calculateHomographyError(const Correspondence2D3D::Set &correspondences,
+                                         const CorrespondenceSampler &correspondence_sampler)
+{
+  /* There is a 3x3 homography matrix, H, that can transform a point from one plane onto a different plane
+   * | u | = k * | H00 H01 H02 | * | x |
+   * | v |       | H10 H11 H12 |   | y |
+   * | 1 |       | H20 H12  1  |   | 1 |
+   *
+   * In our case we have 2 sets of known corresponding planar points: points on the planar target, and points in the image plane
+   * Therefore, there is some matrix, H, which can transform target points into the image plane.
+   * If the target points and camera points actually match, we should be able to:
+   *   1. Calculate H for a subset of corresponding points
+   *   2. Transform the remaining target points by H to obtain estimates of their locations in the image plane
+   *   3. Compare the calculated estimations to the actual image points to make sure they are very close. If they are not close, we know that the correspondences are not valid
+   *
+   * The matrix H has 8 unique values.
+   * These 8 values of the homography matrix can be solved for, given a set of (at least) 8 corresponding planar vectors, by rearranging the above equations:
+   *
+   * A * H = b, where
+   * H = inv(A) * b
+   *   - A is matrix (size 2*n x 8), where n is the number of corresponding vectors
+   *   - H is a vector (size 8 x 1) of the unknown elements of the homography matrix
+   *   - B is a vector (size 2*n x 1) representing the elements of one set of planar vectors
+   *
+   *                  A               *              H    =    b
+   * |-x0 -y0  -1   0    0    0   u0*x0   u0*y0 | * | H00 | = | -u0 |
+   * | 0   0   0  -x0   -y0  -1   v0*x0   v0*y0 | * | H01 | = | -v0 |
+   *                              ...
+   * |-x7 -y7  -1   0    0    0   u7*x7   u7*y7 | * | H20 | = | -u7 |
+   * | 0   0   0  -x7   -y7  -1   v7*x7   v7*y7 | * | H21 | = | -v7 |
+   *
    */
-  Eigen::MatrixXd A(2*sampled_points,8);
 
-  //U,V coordinates of sampled points
-  Eigen::MatrixXd B(1,2 * sampled_points);
-  std::vector<int> selected_point_index;
-  int points_selected = 0;
-  while (points_selected < sampled_points)
+  // Select the points that we want to use to create the H matrix
+  std::vector<std::size_t> sample_correspondence_indices = correspondence_sampler
+                                                             .getSampleCorrespondenceIndices();
+  std::size_t n_samples = sample_correspondence_indices.size();
+
+  // Ensure that there are enough points for testing outside of the sampled set
+  if (correspondences.size() < 2 * n_samples)
   {
-    //add one to prevent divide by zero
-    int corner = points_selected + 1 % 4;
-
-    //Nominally 4 points will be used (minumum requirement) but this will allow as many points as are in the target to be used.
-    ///Does not currently prevent duplicate points
-    switch(corner)
-    {
-      case 1: selected_point_index.push_back(floor(points_selected/4)); //points in the upper left corner; the 1st, 5th, 9th point included
-              break;
-
-      case 2: selected_point_index.push_back(cols - points_selected/4); //Upper right points; 2, 6, 10, etc.
-              break;
-
-      case 3: selected_point_index.push_back(rows*cols - points_selected/4); //lower right points selected; 3, 7, 11
-              break;
-
-      case 4: selected_point_index.push_back((rows-1)*cols + points_selected-1/4); //lower left corner points; 4, 8, 12, etc.
-              break;
-    }
-
-    //switch statement for handling spiral case
-    //select points in spiral from corners
-    points_selected ++;
+    std::stringstream ss;
+    ss << "Correspondences size is not more than 2x sample size (" << correspondences.size()
+       << " correspondences vs. " << n_samples << ")";
+    throw std::runtime_error(ss.str());
   }
 
-  // build matrix of type Ax=b where A x is the unknown elements of the homography matrix "alpha"
-  for(std::size_t i=0; i<selected_point_index.size(); i+=2)
+  // Create the A and b matrices
+  Eigen::MatrixXd A(2 * n_samples, 8);
+  Eigen::MatrixXd b(2 * n_samples, 1);
+
+  // Fill the A and B matrices with data from the selected correspondences
+  for (std::size_t i = 0; i < n_samples; ++i)
   {
-
-    //pt of interest index
-    const rct_optimizations::Correspondence2D3D& pti = ob.correspondence_set[selected_point_index[i]];
-
-    double xi = pti.in_target(0);
-    double yi = pti.in_target(1);
-    //spatial location of target; may be better to pull data from the correspondances
-
-    double Ui = pti.in_target(0);
-    double Vi = pti.in_target(1);
-    //U,V position of the target from the correspondance
+    std::size_t corr_idx = sample_correspondence_indices.at(i);
+    const Correspondence2D3D &corr = correspondences.at(corr_idx);
 
     //assign A row-th row:
-    A.row(i) << -xi, -yi, -1.0, 0.0, 0.0, 0.0, Ui*xi, Ui*yi;
-    B.row(i) << -Ui;
+    const double x = corr.in_target.x();
+    const double y = corr.in_target.y();
+    const double u = corr.in_image.x();
+    const double v = corr.in_image.y();
+    A.row(2 * i) << -x, -y, -1.0, 0.0, 0.0, 0.0, u * x, u * y;
+    A.row(2 * i + 1) << 0.0, 0.0, 0.0, -x, -y, -1.0, v * x, v * y;
 
-    A.row(i+1) << 0.0, 0.0, 0.0, -xi, -yi, -1.0,  Vi*xi, Vi*yi;
-    B.row(i+1) << -Vi;
+    b.block<2, 1>(2 * i, 0) = -1.0 * corr.in_image;
   }
-  //solve with Eigen
 
-  //This Svd methiod can be negetively impacted by using gcc's -ffast-math flag
-  Alpha = A.jacobiSvd().solve(B);
+  // Create the homography matrix
+  Eigen::Matrix<double, 3, 3, Eigen::RowMajor> H = Eigen::Matrix3d::Ones();
 
-  // construct the Homograpahy matrix from Alpha
-  Eigen::Matrix3d P;
-  P << Alpha;
-  P(2,2) = 1.0;
+  // Map the elements of the H matrix into a column vector and solve for the first 8
+  Eigen::Map<Eigen::VectorXd> hv(H.data(), 9);
+  hv.head<8>() = A.fullPivLu().solve(b);
 
-  //Use the mapping constructed in Alpha to verify the remaining points
-  bool rtn = true;
-
-  for(int i=0; i<(int) ob.correspondence_set.size(); i++)
+  // Estimate the image locations of all the target observations and compare to the actual image locations
+  Eigen::VectorXd error(correspondences.size());
+  for (std::size_t i = 0; i < correspondences.size(); ++i)
   {
-    const rct_optimizations::Correspondence2D3D& pti = ob.correspondence_set[i];
+    const Correspondence2D3D &corr = correspondences[i];
 
-    Eigen::Vector2d spatial_coord(pti.in_target(0), pti.in_target(1));
-    Eigen::Vector2d image_coord(pti.in_image(0), pti.in_image(1));
+    // Calculate the scaling factor
+    double ki = 1.0 / (hv(6) * corr.in_target.x() + hv(7) * corr.in_target.y() + 1.0);
 
+    // Replace the z-element of the point with 1
+    Eigen::Vector3d xy(corr.in_target);
+    xy(2) = 1.0;
 
-    double ki = 1.0/(Alpha(6)*spatial_coord(0) + Alpha(7)*spatial_coord(1) + 1.0);
-    //Vector of calculated UV positions;
-    Eigen::Vector3d UV;
-    Eigen::Vector3d X(spatial_coord(0), spatial_coord(1), 1);
+    // Estimate the point in the image plane
+    Eigen::Vector3d in_image_estimate = ki * H * xy;
 
-    UV = ki*P*X;
-
-    Eigen::Vector2d homography_error;
-    homography_error = image_coord - UV.head(2);
-
-    if(fabs(homography_error(0))>max_residual || fabs(homography_error(1))>max_residual)
-    {
-      rtn = false;
-    }
+    // Calculate the error
+    Eigen::Vector2d image_error = corr.in_image - in_image_estimate.head<2>();
+    error(i) = image_error.norm();
   }
 
-  //ave_error = ave_error/(int) ob.correspondence_set.size();
-    return(rtn);
-};
+  return error;
+}
+
+} // namespace rct_optimizations
+
