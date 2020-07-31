bdc64bde288f967a40f69c89169c1417aee0eb6f
Joe Schornak <joe.schornak@gmail.com>
2020-07-31 10:15:20 -0500

    delete duplicate function def; adjust residual threshold in test

--- rct_optimizations/test/include/rct_optimizations_tests/utilities.h
+++ rct_optimizations/test/include/rct_optimizations_tests/utilities.h
@@ -83,14 +83,6 @@ Eigen::Isometry3d perturbPose(const Eigen::Isometry3d &pose,
 DHChain createChain(const Eigen::MatrixXd &dh, const std::vector<DHJointType> &joint_types);
 
 /**
- * @brief Creates a DH chain form
- * @param dh
- * @param joint_types
- * @return
- */
-DHChain createChain(const Eigen::MatrixXd &dh, const std::vector<DHJointType> &joint_types);
-
-/**
  * @brief Creates a DH parameter-based robot representation of an ABB IRB2400
  * @return
  */
