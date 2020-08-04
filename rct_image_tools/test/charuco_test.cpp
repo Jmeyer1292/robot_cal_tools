e7016f43dfe1410c635e494c6dcce90a49c87024
John <john.berkebile@swri.org>
2020-08-04 09:50:01 -0500

    Changed test suite to use the new CharucoGridTarget and removed ROS dependency

--- rct_image_tools/test/charuco_test.cpp
+++ rct_image_tools/test/charuco_test.cpp
@@ -8,130 +8,121 @@
 #include <Eigen/Core>
 #include <iostream>
 #include <gtest/gtest.h>
-#include <opencv2/opencv.hpp>
-#include <ros/package.h>
+#include <rct_image_tools/charuco_grid_target.h>
 
 using namespace cv;
+using namespace rct_image_tools;
 
+// Runs charuco_finder for a completely unobscured ChAruco target. Should detect 24 chessboard corners
 TEST(CharucoChain, TestUnobscured)
 {
     int squaresX = 5;
     int squaresY = 7;
-    float squareLength = 200;
-    float markerLength = 120;
-    int dictionaryId = 10;
+    double squareLength = 0.0200;
+    double markerLength = 0.0120;
 
-    cv::Ptr<cv::aruco::Dictionary> dictionary =
-        cv::aruco::getPredefinedDictionary(cv::aruco::PREDEFINED_DICTIONARY_NAME(dictionaryId));
-
-    // Define Target - create charuco board object
-    cv::Ptr<cv::aruco::CharucoBoard> charucoboard = cv::aruco::CharucoBoard::create(squaresX, squaresY, squareLength, markerLength, dictionary);
-    cv::Ptr<cv::aruco::Board> board = charucoboard.staticCast<cv::aruco::Board>();
-
-    const std::string path = ros::package::getPath("rct_image_tools") + "/test/images/charuco_unobstructed.jpg";
+    // Creating the CharucoGridTarget with the above parameters
+    CharucoGridTarget target(squaresX, squaresY, squareLength, markerLength);
+    const std::string path = std::string(TEST_SUPPORT_DIR) + "charuco_unobscured.jpg";
     //Load Image
     cv::Mat m = imread(path);
     EXPECT_FALSE(m.empty());
 
-    rct_image_tools::CharucoGridBoardObservationFinder charuco_finder(charucoboard);
-
-    std::map<int, std::vector<Eigen::Vector2f>> maybe_charuco = charuco_finder.findObservations(m);
+    rct_image_tools::CharucoGridBoardObservationFinder charuco_finder(target);
+    // Find the chessboard corners (if any)
+    std::map<int, Eigen::Vector2d> maybe_charuco = charuco_finder.findObservations(m);
 
     EXPECT_FALSE(maybe_charuco.empty());
+    // Should have detected 24 corners
     EXPECT_EQ(maybe_charuco.size(), 24);
-
-    auto id_iterator = maybe_charuco.cbegin();
+    // Iterate through the corners to see if they match the ID (should see IDs 0-23)
+    auto id_iterator = maybe_charuco.begin();
     for(int i = 0; i < maybe_charuco.size(); i++)
     {
-      int key_id = id_iterator->first;
-      EXPECT_EQ(i, key_id);
+      EXPECT_EQ(i, id_iterator->first);
       std::advance(id_iterator, 1);
     }
     return;
 }
 
+// Runs charuco_finder for a partially obscured (blocked IDs 0-6) ChAruco target. Should detect 17 chessboard corners
 TEST(CharucoChain, TestObscured)
 {
     int squaresX = 5;
     int squaresY = 7;
-    float squareLength = 200;
-    float markerLength = 120;
-    int dictionaryId = 10;
+    double squareLength = 0.0200;
+    double markerLength = 0.0120;
 
-    cv::Ptr<cv::aruco::Dictionary> dictionary =
-        cv::aruco::getPredefinedDictionary(cv::aruco::PREDEFINED_DICTIONARY_NAME(dictionaryId));
+    // Creating the CharucoGridTarget with the above parameters
+    CharucoGridTarget target(squaresX, squaresY, squareLength, markerLength);
 
-    // Define Target - create charuco board object
-    cv::Ptr<cv::aruco::CharucoBoard> charucoboard = cv::aruco::CharucoBoard::create(squaresX, squaresY, squareLength, markerLength, dictionary);
-    cv::Ptr<cv::aruco::Board> board = charucoboard.staticCast<cv::aruco::Board>();
-
-    const std::string path = ros::package::getPath("rct_image_tools") + "/test/images/charuco_obstructed.jpg";
+    const std::string path = std::string(TEST_SUPPORT_DIR) + "charuco_obscured.jpg";
     //Load Image
     cv::Mat m = imread(path);
     EXPECT_FALSE(m.empty());
 
-    rct_image_tools::CharucoGridBoardObservationFinder charuco_finder(charucoboard);
-
-    std::map<int, std::vector<Eigen::Vector2f>> maybe_charuco = charuco_finder.findObservations(m);
+    rct_image_tools::CharucoGridBoardObservationFinder charuco_finder(target);
+    // Find the chessboard corners (if any)
+    std::map<int,Eigen::Vector2d> maybe_charuco = charuco_finder.findObservations(m);
 
     EXPECT_FALSE(maybe_charuco.empty());
+    // Should have detected 17 corners
     EXPECT_EQ(maybe_charuco.size(), 17);
-
-    auto id_iterator = maybe_charuco.cbegin();
+    // Iterate through the corners to see if they match the ID (should see IDs 7-23)
+    auto id_iterator = maybe_charuco.begin();
     for(int i = 0; i < maybe_charuco.size(); i++)
     {
-      int key_id = id_iterator->first;
+      // These IDs were obscured so they shouldn't have been detected
       if(i < 7)
       {
-        EXPECT_NE(i, key_id);
+        EXPECT_NE(i, id_iterator->first);
       }
+      // The rest of the IDs should've been detected
       else
       {
-        EXPECT_EQ(i, key_id);
+        EXPECT_EQ(i, id_iterator->first);
         std::advance(id_iterator, 1);
       }
     }
     return;
 }
 
+// Runs charuco_finder for an almost entirely obscured (blocked IDs 1-23) ChAruco target. Should detect only 1 chessboard corner
 TEST(CharucoChain, TestOneCorner)
 {
     int squaresX = 5;
     int squaresY = 7;
-    float squareLength = 200;
-    float markerLength = 120;
-    int dictionaryId = 10;
+    double squareLength = 0.0200;
+    double markerLength = 0.0120;
 
-    cv::Ptr<cv::aruco::Dictionary> dictionary =
-        cv::aruco::getPredefinedDictionary(cv::aruco::PREDEFINED_DICTIONARY_NAME(dictionaryId));
+    // Creating the CharucoGridTarget with the above parameters
+    CharucoGridTarget target(squaresX, squaresY, squareLength, markerLength);
 
-    // Define Target - create charuco board object
-    cv::Ptr<cv::aruco::CharucoBoard> charucoboard = cv::aruco::CharucoBoard::create(squaresX, squaresY, squareLength, markerLength, dictionary);
-    cv::Ptr<cv::aruco::Board> board = charucoboard.staticCast<cv::aruco::Board>();
-
-    const std::string path = ros::package::getPath("rct_image_tools") + "/test/images/one_corner.jpg";
+    const std::string path = std::string(TEST_SUPPORT_DIR) + "one_corner.jpg";
     //Load Image
     cv::Mat m = imread(path);
     EXPECT_FALSE(m.empty());
 
-    rct_image_tools::CharucoGridBoardObservationFinder charuco_finder(charucoboard);
-
-    std::map<int, std::vector<Eigen::Vector2f>> maybe_charuco = charuco_finder.findObservations(m);
+    rct_image_tools::CharucoGridBoardObservationFinder charuco_finder(target);
+    // Find the chessboard corners (if any)
+    std::map<int, Eigen::Vector2d> maybe_charuco = charuco_finder.findObservations(m);
 
     EXPECT_FALSE(maybe_charuco.empty());
+    // Should have detected only one corner
     EXPECT_EQ(maybe_charuco.size(), 1);
-
-    auto id_iterator = maybe_charuco.cbegin();
+    // Iterate through the corners to see if they match the ID (should see only ID 0)
+    auto id_iterator = maybe_charuco.begin();
     for(int i = 0; i < maybe_charuco.size(); i++)
     {
-      int key_id = id_iterator->first;
+      // The only corner that wasn't obscured should be detected
       if(i < 1)
       {
-        EXPECT_EQ(i, key_id);
+        EXPECT_EQ(i, id_iterator->first);
       }
+      // The rest were obscured and thus shouldn't be detected
       else
       {
-        EXPECT_NE(i, key_id);
+        EXPECT_NE(i, id_iterator->first);
       }
       std::advance(id_iterator, 1);
     }
