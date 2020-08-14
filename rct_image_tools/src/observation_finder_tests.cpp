4b203ceaed655a4f04bf6db09f481242970a0df4
mripperger <michael.ripperger@swri.org>
2020-08-13 20:58:18 -0500

    Updated image observation finder test node

--- rct_image_tools/src/observation_finder_tests.cpp
+++ rct_image_tools/src/observation_finder_tests.cpp
@@ -23,7 +23,7 @@ int main(int argc, char** argv)
   rct_image_tools::ModifiedCircleGridTarget target (rows, cols, 0.01);
 
   // Create a finder that works with this target
-  rct_image_tools::ModifiedCircleGridObservationFinder obs_finder(target);
+  rct_image_tools::ModifiedCircleGridObservationFinder obs_finder(target, true);
 
   // Attempt to find the grid: The optional will be set if it succeeded.
   boost::optional<std::vector<Eigen::Vector2d>> maybe_obs = obs_finder.findObservations(m);
