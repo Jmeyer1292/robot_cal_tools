497c13c0438a0536e5041588e312badd6a1154db
mripperger <michael.ripperger@swri.org>
2020-08-19 11:55:43 -0500

    Updated kinematic calibration tool with better testing

--- rct_examples/src/tools/kinematic_calibration.cpp
+++ rct_examples/src/tools/kinematic_calibration.cpp
@@ -1,12 +1,11 @@
 #include <rct_optimizations/dh_chain_kinematic_calibration.h>
 #include <rct_ros_tools/parameter_loaders.h>
+#include <rct_ros_tools/exceptions.h>
 
 #include <boost/accumulators/accumulators.hpp>
 #include <boost/accumulators/statistics.hpp>
-#include <yaml-cpp/yaml.h>
-#include <ros/ros.h>
-
 #include <random>
+#include <yaml-cpp/yaml.h>
 
 using namespace rct_optimizations;
 using namespace rct_ros_tools;
@@ -45,24 +44,26 @@ KinematicMeasurement::Set loadMeasurements(const std::string& filename)
       }
 
       // Camera to target pose
-      YAML::Node pose = it->second["pose"];
-      double x = pose["x"].as<double>();
-      double y = pose["y"].as<double>();
-      double z = pose["z"].as<double>();
-      double rx = pose["rx"].as<double>();
-      double ry = pose["ry"].as<double>();
-      double rz = pose["rz"].as<double>();
+      {
+        YAML::Node pose = it->second["pose"];
+        double x = pose["x"].as<double>();
+        double y = pose["y"].as<double>();
+        double z = pose["z"].as<double>();
+        double rx = pose["rx"].as<double>();
+        double ry = pose["ry"].as<double>();
+        double rz = pose["rz"].as<double>();
 
-      measurement.camera_to_target = Eigen::Isometry3d::Identity();
-      measurement.camera_to_target.translate(Eigen::Vector3d(x, y, z));
+        measurement.camera_to_target = Eigen::Isometry3d::Identity();
+        measurement.camera_to_target.translate(Eigen::Vector3d(x, y, z));
 
-      // Euler XYZ
-      measurement.camera_to_target.rotate(Eigen::AngleAxisd(rx, Eigen::Vector3d::UnitX()));
-      measurement.camera_to_target.rotate(Eigen::AngleAxisd(ry, Eigen::Vector3d::UnitY()));
-      measurement.camera_to_target.rotate(Eigen::AngleAxisd(rz, Eigen::Vector3d::UnitZ()));
+        // Euler XYZ
+        measurement.camera_to_target.rotate(Eigen::AngleAxisd(rx, Eigen::Vector3d::UnitX()));
+        measurement.camera_to_target.rotate(Eigen::AngleAxisd(ry, Eigen::Vector3d::UnitY()));
+        measurement.camera_to_target.rotate(Eigen::AngleAxisd(rz, Eigen::Vector3d::UnitZ()));
 
-      // Add the measurement to the set
-      measurements.push_back(measurement);
+        // Add the measurement to the set
+        measurements.push_back(measurement);
+      }
     }
   }
   catch (YAML::Exception& ex)
@@ -79,7 +80,7 @@ std::pair<KinematicMeasurement::Set, KinematicMeasurement::Set> divide(const Kin
   std::vector<std::size_t> cal_indices(measurements.size());
   std::iota(cal_indices.begin(), cal_indices.end(), 0);
   std::mt19937 mt_rand(std::random_device{}());
-  std::random_shuffle(cal_indices.begin(), cal_indices.end()); //, mt_rand);
+  std::shuffle(cal_indices.begin(), cal_indices.end(), mt_rand);
 
   std::size_t n = static_cast<std::size_t>(static_cast<double>(measurements.size()) * training_pct);
 
@@ -134,8 +135,53 @@ DHChain createTwoAxisPositioner()
   return DHChain(transforms, base_offset);
 }
 
-void test(const DHChain& initial_camera_chain, const DHChain& initial_target_chain,
-          const KinematicCalibrationResult& result, const KinematicMeasurement::Set& measurements)
+void printResults(const KinematicCalibrationResult& result)
+{
+  Eigen::IOFormat fmt(4, 0, "|", "\n", "|", "|");
+
+  std::stringstream ss;
+  ss << "\nCalibration " << (result.converged ? "did" : "did not") << " converge\n";
+  ss << "Initial cost per observation: " << std::sqrt(result.initial_cost_per_obs) << "\n";
+  ss << "Final cost per observation: " << std::sqrt(result.final_cost_per_obs) << "\n";
+
+  ss << "\nCamera mount to camera\n" << result.camera_mount_to_camera.matrix().format(fmt) << "\n";
+  ss << "Euler XYZ: " << result.camera_mount_to_camera.rotation().eulerAngles(0, 1, 2).transpose().format(fmt) << "\n";
+
+  ss << "\nTarget mount to target\n" << result.target_mount_to_target.matrix().format(fmt) << "\n";
+  ss << "Euler XYZ: " << result.target_mount_to_target.rotation().eulerAngles(0, 1, 2).transpose().format(fmt) << "\n";
+
+  ss << "\nDH parameter offsets\n" << result.target_chain_dh_offsets.matrix().format(fmt) << "\n";
+  ss << result.covariance.printCorrelationCoeffAboveThreshold(0.5);
+
+  std::cout << ss.str() << std::endl;
+}
+
+struct Stats
+{
+  double pos_mean;
+  double pos_stdev;
+  double rot_mean;
+  double rot_stdev;
+
+  void print() const
+  {
+    std::cout << "Position Difference Mean: " << pos_mean << std::endl;
+    std::cout << "Position Difference Std. Dev.: " << pos_stdev << std::endl;
+    std::cout << "Orientation Difference Mean: " << rot_mean << std::endl;
+    std::cout << "Orientation difference Std. Dev.: " << rot_stdev << "\n" << std::endl;
+  }
+
+  void percentDiff(const Stats& other)
+  {
+    std::cout << "Position: " << 100.0 * (pos_mean - other.pos_mean) / pos_mean << "%" << std::endl;
+    std::cout << "Position Std. Dev.: " << 100.0 * (pos_stdev - other.pos_stdev) / pos_stdev << "%" << std::endl;
+    std::cout << "Orientation: " << 100.0 * (rot_mean - other.rot_mean) / rot_mean << "%" << std::endl;
+    std::cout << "Orientation Std. Dev.: " << 100.0 * (rot_stdev - other.rot_stdev) / rot_stdev << "%" << "\n" << std::endl;
+  }
+};
+
+Stats compareToMeasurements(const DHChain& initial_camera_chain, const DHChain& initial_target_chain,
+                            const KinematicCalibrationResult& result, const KinematicMeasurement::Set& measurements)
 {
   // Test the result by moving the robot around to a lot of positions and seeing of the results match
   DHChain camera_chain(initial_camera_chain, result.camera_chain_dh_offsets);
@@ -165,11 +211,13 @@ void test(const DHChain& initial_camera_chain, const DHChain& initial_target_cha
     ori_acc(Eigen::Quaterniond(camera_to_target.linear()).angularDistance(Eigen::Quaterniond(m.camera_to_target.linear())));
   }
 
-  std::cout << "Position Difference Mean: " << ba::mean(pos_acc) << std::endl;
-  std::cout << "Position Difference Std. Dev.: " << std::sqrt(ba::variance(pos_acc)) << std::endl;
+  Stats stats;
+  stats.pos_mean = ba::mean(pos_acc);
+  stats.pos_stdev = std::sqrt(ba::variance(pos_acc));
+  stats.rot_mean = ba::mean(ori_acc);
+  stats.rot_stdev = std::sqrt(ba::variance(ori_acc));
 
-  std::cout << "Orientation Difference Mean: " << ba::mean(ori_acc) << std::endl;
-  std::cout << "Orientation difference Std. Dev.: " << std::sqrt(ba::variance(ori_acc)) << std::endl;
+  return stats;
 }
 
 template <typename T>
@@ -185,22 +233,13 @@ bool get(const ros::NodeHandle& nh, const std::string& key, T& val)
 
 int main(int argc, char** argv)
 {
-  //  ros::init(argc, argv, "kinematic_calibration");
-  //  ros::NodeHandle pnh("~");
-
-  //  std::string data_file;
-  //  if (!get(pnh, "data_file", data_file))
-  //    return -1;
-
-
   // Load the observations
   KinematicMeasurement::Set measurements = loadMeasurements(argv[1]);
 
-  // Split the observations into a training and validation group
-  std::pair<KinematicMeasurement::Set, KinematicMeasurement::Set> measurement_sets = divide(measurements, 0.8);
-
   KinematicCalibrationProblemPose3D problem(DHChain({}), createTwoAxisPositioner());
-  problem.observations = measurement_sets.first;
+
+  problem.observations = measurements;
+
   problem.target_mount_to_target_guess = Eigen::Isometry3d::Identity();
   problem.target_mount_to_target_guess.translate(Eigen::Vector3d(0.17, -0.65, 0.5));
   problem.target_mount_to_target_guess.rotate(Eigen::AngleAxisd(M_PI_2, Eigen::Vector3d::UnitX()));
@@ -235,30 +274,42 @@ int main(int argc, char** argv)
   // Mask the z-value of the camera mount to camera position
 //  problem.mask.at(4) = { 0, 2 };
 
-  KinematicCalibrationResult result = optimize(problem);
+  // Run the calibration
+  Stats cal_stats_optimal_dh;
+  {
+    KinematicCalibrationResult result = optimize(problem);
+    printResults(result);
 
-  Eigen::IOFormat fmt(4, 0, "|", "\n", "|", "|");
+    //  test(problem.camera_chain, problem.target_chain, result, measurement_sets.second);
+    cal_stats_optimal_dh = compareToMeasurements(problem.camera_chain, problem.target_chain, result, measurements);
 
-  std::stringstream ss;
-  ss << "\nCalibration " << (result.converged ? "did" : "did not") << " converge\n";
-  ss << "Initial cost per observation: " << std::sqrt(result.initial_cost_per_obs) << "\n";
-  ss << "Final cost per observation: " << std::sqrt(result.final_cost_per_obs) << "\n";
-  ss << "\nCamera mount to camera\n" << result.camera_mount_to_camera.matrix().format(fmt) << "\n";
+    std::cout << "DH calibration validation:" << std::endl;
+    cal_stats_optimal_dh.print();
+  }
 
-  ss << result.camera_mount_to_camera.rotation().eulerAngles(0, 1, 2) << "\n";
+  // Re-run the calibration without optimizing the chain parameters
+  Stats cal_stats_static_dh;
+  {
+    // Mask the entire target chain
+    {
+      Eigen::Matrix<bool, Eigen::Dynamic, 4> mask =
+          Eigen::Matrix<bool, Eigen::Dynamic, 4>::Constant(problem.target_chain.dof(), 4, true);
 
+      problem.mask.at(1) = createDHMask(mask);
+    }
 
-  ss << "\nTarget mount to target\n" << result.target_mount_to_target.matrix().format(fmt) << "\n";
+    KinematicCalibrationResult result = optimize(problem);
+    printResults(result);
 
-  ss << result.target_mount_to_target.rotation().eulerAngles(0, 1, 2) << "\n";
+    // Compare the results of this optimization with the measurements using the measured joints and nominal kinematic chain
+    cal_stats_static_dh = compareToMeasurements(problem.camera_chain, problem.target_chain, result, measurements);
+    std::cout << "Calibration validation - static DH parameters:" << std::endl;
+    cal_stats_static_dh.print();
+  }
 
-
-  ss << "\nDH parameter offsets\n" << result.target_chain_dh_offsets.matrix().format(fmt) << "\n";
-  ss << result.covariance.printCorrelationCoeffAboveThreshold(0.5);
-
-  std::cout << ss.str() << std::endl;
-
-  test(problem.camera_chain, problem.target_chain, result, measurement_sets.second);
+  // Print the percentage difference between the calibrations
+  std::cout << "Percent improvement: calibration vs. nominal kinematic model" << std::endl;
+  cal_stats_static_dh.percentDiff(cal_stats_optimal_dh);
 
   return 0;
 }
