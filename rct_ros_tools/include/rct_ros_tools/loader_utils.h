ca1cc9786f07d1df44cee83e88051086c838ed0d
Michael Ripperger <michael.ripperger@swri.org>
2021-08-11 17:40:57 -0500

    Added utility to convert between YAML and XmlRpcValue

--- rct_ros_tools/include/rct_ros_tools/loader_utils.h
+++ rct_ros_tools/include/rct_ros_tools/loader_utils.h
@@ -1,24 +1,15 @@
 #pragma once
 
+#include <yaml-cpp/node/node.h>
 #include <xmlrpcpp/XmlRpcValue.h>
-#include <xmlrpcpp/XmlRpcException.h>
-#include <yaml-cpp/yaml.h>
-#include <ros/console.h>
 
 namespace rct_ros_tools
 {
+/**
+ * @brief Converts an XmlRpcValue object into a YAML node
+ * @param value
+ * @return
+ */
+YAML::Node toYAML(const XmlRpc::XmlRpcValue& value);
 
-template<typename T>
-static bool read(XmlRpc::XmlRpcValue& xml, const std::string& key, T& value)
-{
-  if (!xml.hasMember(key)) return false;
-  try {
-    value = static_cast<T>(xml[key]);
-  } catch (const XmlRpc::XmlRpcException& ex) {
-    ROS_ERROR_STREAM(ex.getMessage());
-    return false;
-  }
-  return true;
-}
-
-}
+} // namespace rct_ros_tools
