e9109faa5d37ca2012990ea54e3d042fa29573e8
Michael Ripperger <michael.ripperger@swri.org>
2021-09-30 12:08:23 -0500

    Added serialization for DH types

--- rct_optimizations/include/rct_optimizations/serialization/types.h
+++ rct_optimizations/include/rct_optimizations/serialization/types.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <rct_optimizations/dh_chain.h>
 #include <rct_optimizations/types.h>
 #include <rct_optimizations/serialization/eigen.h>
 
@@ -81,6 +82,64 @@ struct convert<rct_optimizations::Observation<SENSOR_DIM, WORLD_DIM>>
     obs.correspondence_set = node["correspondences"].as<decltype(obs.correspondence_set)>();
     obs.to_target_mount = node["to_target_mount"].as<decltype(obs.to_target_mount)>();
     obs.to_camera_mount = node["to_camera_mount"].as<decltype(obs.to_camera_mount)>();
+
+    return true;
+  }
+};
+
+template <>
+struct convert<rct_optimizations::DHTransform>
+{
+  using T = rct_optimizations::DHTransform;
+  static bool decode(const Node& n, T& val)
+  {
+    // Joint type
+    switch (static_cast<rct_optimizations::DHJointType>(n["type"].as<unsigned>()))
+    {
+      case rct_optimizations::DHJointType::LINEAR:
+        val.type = rct_optimizations::DHJointType::LINEAR;
+        break;
+      case rct_optimizations::DHJointType::REVOLUTE:
+        val.type = rct_optimizations::DHJointType::REVOLUTE;
+        break;
+      default:
+        throw std::runtime_error("Invalid joint type");
+    }
+
+    // DH offsets
+    {
+      YAML::Node d = n["d"];
+      YAML::Node theta = n["theta"];
+      YAML::Node r = n["r"];
+      YAML::Node alpha = n["alpha"];
+
+      Eigen::Vector4d params;
+      params << d.as<double>(), theta.as<double>(), r.as<double>(), alpha.as<double>();
+
+      val.params = params;
+    }
+
+    val.name = n["joint_name"].as<std::string>();
+
+    return true;
+  }
+};
+
+template <>
+struct convert<rct_optimizations::DHChain>
+{
+  using T = rct_optimizations::DHChain;
+  inline static bool decode(const Node& n, T& val)
+  {
+    val.base_offset_ = n["base_transform"].as<Eigen::Isometry3d>();
+
+    const YAML::Node& transforms = n["dh_transforms"];
+    val.transforms_.clear();
+    val.transforms_.reserve(transforms.size());
+    for (std::size_t i = 0; i < transforms.size(); i++)
+    {
+      val.transforms_.push_back(transforms[i].as<rct_optimizations::DHTransform>());
+    }
 
     return true;
   }
